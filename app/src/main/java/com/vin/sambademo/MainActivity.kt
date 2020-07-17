5efdb29666182646368d08ad68fc7016134131f0
vineed <vineedgangadharan@gmail.com>
2020-07-17 19:15:17 +0530

    update

--- app/src/main/java/com/vin/sambademo/MainActivity.kt
+++ app/src/main/java/com/vin/sambademo/MainActivity.kt
@@ -2,9 +2,12 @@ package com.vin.sambademo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.hierynomus.smbj.SMBClient
+import com.hierynomus.smbj.auth.AuthenticationContext
+import com.hierynomus.smbj.session.Session
+import com.hierynomus.smbj.share.DiskShare
 import jcifs.CIFSContext
 import jcifs.context.SingletonContext
-import jcifs.smb.NtlmPasswordAuthentication
 import jcifs.smb.SmbException
 import jcifs.smb.SmbFile
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,6 +15,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.IOException
 import java.net.MalformedURLException
 
 
@@ -22,7 +26,27 @@ class MainActivity : AppCompatActivity() {
 
         bFiles.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
-                loadSamba()
+                loadSambaWithSMBJ()
+            }
+        }
+    }
+
+    private suspend fun loadSambaWithSMBJ() {
+        val client = SMBClient()
+
+        client.connect("SERVERNAME").use { connection ->
+            val ac = AuthenticationContext.anonymous()
+            //AuthenticationContext("", "".toCharArray(), "")
+            val session: Session = connection.authenticate(ac)
+
+            // Connect to Share
+            val diskShare = session.connectShare("forbes_logo") as DiskShare?
+
+            diskShare?.let {
+                for (f in it.list("FOLDER", "*.TXT")) {
+                    println("File : " + f.fileName)
+                    appendTextWithContext("File : " + f.fileName)
+                }
             }
         }
     }
@@ -44,7 +68,7 @@ class MainActivity : AppCompatActivity() {
                 val servers = domains[i].listFiles()
                 for (j in servers.indices) {
                     println("\t" + servers[j])
-                    appendText("\t" + servers[j])
+                    appendTextWithContext("\t" + servers[j])
                 }
             }
         } catch (e: SmbException) {
@@ -57,7 +81,11 @@ class MainActivity : AppCompatActivity() {
         //------------------------[/code]----------------------------------------//
     }
 
-    private suspend fun appendText(text: String) = withContext(Dispatchers.Main) {
+    private suspend fun appendTextWithContext(text: String) = withContext(Dispatchers.Main) {
+        appendText(text)
+    }
+
+    private fun appendText(text: String) {
         tvMsg.append(text + "\n")
     }
 }
