c5d79bbcc07b69b36f6c6274b2ef1552a01dcd1f
vineed.gangadharan@forbestechnosys.com <dhanashreemandle@gmail.com>
2020-07-17 19:31:47 +0530

    updaate

--- app/src/main/java/com/vin/sambademo/MainActivity.kt
+++ app/src/main/java/com/vin/sambademo/MainActivity.kt
@@ -6,10 +6,6 @@ import com.hierynomus.smbj.SMBClient
 import com.hierynomus.smbj.auth.AuthenticationContext
 import com.hierynomus.smbj.session.Session
 import com.hierynomus.smbj.share.DiskShare
-import jcifs.CIFSContext
-import jcifs.context.SingletonContext
-import jcifs.smb.SmbException
-import jcifs.smb.SmbFile
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -34,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     private suspend fun loadSambaWithSMBJ() {
         val client = SMBClient()
 
-        client.connect("SERVERNAME").use { connection ->
+        client.connect("192.168.2.190").use { connection ->
             val ac = AuthenticationContext.anonymous()
             //AuthenticationContext("", "".toCharArray(), "")
             val session: Session = connection.authenticate(ac)
@@ -43,42 +39,12 @@ class MainActivity : AppCompatActivity() {
             val diskShare = session.connectShare("forbes_logo") as DiskShare?
 
             diskShare?.let {
-                for (f in it.list("FOLDER", "*.TXT")) {
+                for (f in it.list("")) {
                     println("File : " + f.fileName)
                     appendTextWithContext("File : " + f.fileName)
                 }
             }
         }
-    }
-
-    private suspend fun loadSamba() {
-        //-----------------------[code]---------------------------------//
-        try {
-            val base: CIFSContext = SingletonContext.getInstance()
-            val auth = base.withAnonymousCredentials()
-            /*val authed1 =
-                base.withCredentials(NtlmPasswordAuthentication(base, "", "", ""))*/
-
-            val domains: Array<SmbFile> =
-                SmbFile("smb://192.168.2.190/forbes_logo", auth).listFiles()
-
-            for (i in domains.indices) {
-                println(domains[i])
-                appendText(domains[i].toString())
-                val servers = domains[i].listFiles()
-                for (j in servers.indices) {
-                    println("\t" + servers[j])
-                    appendTextWithContext("\t" + servers[j])
-                }
-            }
-        } catch (e: SmbException) {
-            e.printStackTrace()
-            appendText(e.message ?: "Exception")
-        } catch (e: MalformedURLException) {
-            e.printStackTrace()
-            appendText(e.message ?: "Exception")
-        }
-        //------------------------[/code]----------------------------------------//
     }
 
     private suspend fun appendTextWithContext(text: String) = withContext(Dispatchers.Main) {
